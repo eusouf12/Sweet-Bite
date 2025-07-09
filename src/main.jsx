b18333c0838339e716fc2727aedd27b4acc7f7c4
Eusouf <riyajuddeneusouf@gmail.com>
2025-07-09 21:16:12 +0600

    error element add

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,13 @@ import Root from './Component/Layout/Root.jsx';
 import Home from "./Component/Pages/Home/Home.jsx";
 import Login from "./Component/Pages/Login/Login.jsx";
 import Register from "./Component/Pages/Register/Register.jsx";
+import Error from "./Component/Pages/Error/Error.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <Error></Error>,
     children: [
       {
         path: '/',
